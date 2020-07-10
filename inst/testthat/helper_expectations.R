ba0bbc8460a5126ac7163dcf9fbac9d6b46b8ea5
damirpolat <dpulatov@uwyo.edu>
2020-07-10 14:22:25 -0600

    added basic prediction test

--- inst/testthat/helper_expectations.R
+++ inst/testthat/helper_expectations.R
@@ -375,6 +375,12 @@ expect_prediction = function(p) {
 
 expect_prediction_clust = function(p) {
   expect_prediction(p)
+  checkmate::expect_r6(p, "PredictionClust", public = c("row_ids", "truth", "predict_types", "prob", "partition"))
+  checkmate::expect_numeric(p$truth, any.missing = TRUE, len = length(p$row_ids), null.ok = TRUE)
+  checkmate::expect_numeric(p$partition, any.missing = FALSE, len = length(p$row_ids), null.ok = TRUE)
+  if ("prob" %in% p$predict_types) {
+    checkmate::expect_numeric(p$prob, any.missing = FALSE, len = length(p$row_ids), lower = 0)
+  }
 }
 
 expect_resample_result = function(rr, allow_incomplete = FALSE) {
