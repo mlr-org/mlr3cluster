2c2ea5b33e195213fcf813a5675dce28c57d2ebf
damirpolat <dpulatov@uwyo.edu>
2020-07-17 10:38:18 -0600

    fixed most style warnings

--- tests/testthat/test_TaskClust.R
+++ tests/testthat/test_TaskClust.R
@@ -6,19 +6,3 @@ test_that("Basic ops on usarrests task", {
   expect_task_clust(task)
   expect_identical(task$target_names, character(0))
 })
-
-test_that("0 feature task", {
-  b = as_data_backend(iris[, 5L, drop = FALSE])
-  task = TaskClassif$new(id = "zero_feat_task", b, target = "Species")
-  expect_output(print(task))
-  b = task$backend
-  expect_backend(b)
-  expect_task(task)
-  expect_task_supervised(task)
-  expect_task_classif(task)
-  expect_data_table(task$data(), ncols = 1L)
-
-  lrn = lrn("classif.featureless")
-  p = lrn$train(task)$predict(task)
-  expect_prediction(p)
-})
