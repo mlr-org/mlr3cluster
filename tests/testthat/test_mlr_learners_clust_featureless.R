9913235330c11520d2085e5c9aa90c9cce54f706
damirpolat <dpulatov@uwyo.edu>
2020-07-20 14:50:49 -0600

    removed useless test for clust featureless

--- tests/testthat/test_mlr_learners_clust_featureless.R
+++ tests/testthat/test_mlr_learners_clust_featureless.R
@@ -4,22 +4,6 @@ test_that("autotest", {
   learner = lrn("clust.featureless")
   expect_learner(learner)
 
-  # skip everything for now
-  exclude = "(sanity|feat_single_logical|feat_single_integer|feat_single_numeric|"
-  exclude = paste(exclude, "feat_all|missings)", sep = "")
-  result = run_autotest(learner, exclude = exclude)
+  result = run_autotest(learner)
   expect_true(result, info = result$error)
-})
-
-test_that("Simple training/predict", {
-  task = tsk("usarrests")
-  learner = lrn("clust.featureless")
-  expect_learner(learner, task)
-
-  learner$train(task)
-  learner$predict(task)
-  expect_class(learner$model, "clust.featureless_model")
-  prediction = learner$predict(task)
-  perf = prediction$score(msr("clust.silhouette"), task)
-  expect_number(perf, lower = 1, upper = 1)
 })
