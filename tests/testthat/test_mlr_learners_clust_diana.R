9291516ef1fd27d48c2abc9c2bb9cddac8319d14
damirpolat <dpulatov@uwyo.edu>
2020-07-23 13:49:54 -0600

    added basic tests for diana

---
+++ tests/testthat/test_mlr_learners_clust_diana.R
@@ -0,0 +1,28 @@
+context("clust.diana")
+
+skip_if_not_installed("clue")
+
+test_that("autotest", {
+  learner = mlr3::lrn("clust.diana")
+  expect_learner(learner)
+
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
+
+
+test_that("Learner properties are respected", {
+  task = tsk("usarrests")
+  learner = mlr_learners$get("clust.diana")
+  expect_learner(learner, task)
+
+  p = learner$train(task)$predict(task)
+  expect_prediction_clust(p)
+
+  if ("complete" %in% learner$properties) {
+    expect_prediction_complete(p, learner$predict_type)
+  }
+  if ("exclusive" %in% learner$properties) {
+    expect_prediction_exclusive(p, learner$predict_type)
+  }
+})
