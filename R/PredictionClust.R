a0ad26150f03e30b7a26be5e533cd7734f3dcfdf
Michel Lang <michellang@gmail.com>
2020-09-07 15:52:22 +0200

    ...

--- R/PredictionClust.R
+++ R/PredictionClust.R
@@ -27,67 +27,29 @@ PredictionClust = R6Class("PredictionClust",
     #'   Columns must be named with cluster numbers, row names are automatically removed.
     #'   If `prob` is provided, but `partition` is not, the cluster memberships are calculated from
     #'   the probabilities using [max.col()] with `ties.method` set to `"first"`.
-    initialize = function(task = NULL, row_ids = task$row_ids, partition = NULL, prob = NULL) {
+    initialize = function(task = NULL, row_ids = task$row_ids, partition = NULL, prob = NULL, check = TRUE) {
+      pdata = list(row_ids = row_ids, partition = partition, prob = prob)
+      pdata = discard(pdata, is.null)
+      class(pdata) = c("PredictionDataClust", "PredictionData")
 
-      assert_row_ids(row_ids)
-      n = length(row_ids)
-
-      self$task_type = "clust"
-
-      if (!is.null(partition)) {
-        partition = assert_integer(partition, len = n, any.missing = FALSE)
+      if (check) {
+        pdata = check_prediction_data(pdata)
       }
-
-      if (!is.null(prob)) {
-        # need to check number of columns for matrix
-        assert_matrix(prob, nrows = n)
-        assert_numeric(prob, lower = 0, upper = 1)
-        if (!is.null(rownames(prob))) {
-          rownames(prob) = NULL
-        }
-
-        if (is.null(partition)) {
-          # calculate partition from prob
-          partition = max.col(prob, ties.method = "first")
-        }
-      }
-
-      # Check returned predict types have correct names and add to data.table
-      self$predict_types = c("partition", "prob")[c(!is.null(partition), !is.null(prob))]
-      self$data$tab = data.table(
-        row_id = row_ids,
-        partition = partition
-      )
-      self$data$prob = prob
+      self$data = pdata
+      self$predict_types = intersect(c("partition", "prob"), names(pdata))
     }
   ),
   active = list(
     #' @field partition (`integer()`)\cr
     #' Access the stored partition.
     partition = function() {
-      self$data$tab$partition %??% rep(NA_real_, length(self$data$row_ids))
+      self$data$partition %??% rep(NA_real_, length(self$data$row_ids))
     },
 
     #' @field prob (`matrix()`)\cr
     #' Access to the stored probabilities.
     prob = function() {
       self$data$prob
-    },
-
-    #' @field missing (`integer()`)\cr
-    #' Returns `row_ids` for which the predictions are missing or incomplete.
-    missing = function() {
-      miss = logical(nrow(self$data$tab))
-
-      if ("partition" %in% self$predict_types) {
-        miss = miss | is.na(self$data$tab$partition)
-      }
-
-      if ("prob" %in% self$predict_types) {
-        miss = miss | apply(self$data$prob, 1L, anyMissing)
-      }
-
-      self$data$tab$row_id[miss]
     }
   )
 )
@@ -95,7 +57,7 @@ PredictionClust = R6Class("PredictionClust",
 
 #' @export
 as.data.table.PredictionClust = function(x, ...) { #nolint
-  tab = copy(x$data$tab)
+  tab = as.data.table(x$data[c("row_ids", "partition")])
   if ("prob" %in% x$predict_types) {
     prob = as.data.table(x$data$prob)
     setnames(prob, names(prob), paste0("prob.", names(prob)))
@@ -103,31 +65,4 @@ as.data.table.PredictionClust = function(x, ...) { #nolint
   }
 
   tab
-}
-
-#' @export
-c.PredictionClust = function(..., keep_duplicates = TRUE) {
-
-  dots = list(...)
-  assert_list(dots, "PredictionClust")
-  assert_flag(keep_duplicates)
-  if (length(dots) == 1L || TRUE) {
-    return(dots[[1L]])
-  }
-
-  predict_types = map(dots, "predict_types")
-  if (!every(predict_types[-1L], setequal, y = predict_types[[1L]])) {
-    stopf("Cannot rbind predictions: Different predict_types in objects.")
-  }
-
-  tab = map_dtr(dots, function(p) subset(p$data$tab), .fill = FALSE)
-  prob = do.call(rbind, map(dots, "prob"))
-
-  if (!keep_duplicates) {
-    keep = !duplicated(tab, by = "row_id", fromLast = TRUE)
-    tab = tab[keep]
-    prob = prob[keep, , drop = FALSE]
-  }
-
-  PredictionClust$new(row_ids = tab$row_id, partition = tab$partition, prob = prob)
 }
---
+++ R/PredictionDataClust.R
@@ -0,0 +1,76 @@
+#' @export
+as_prediction.PredictionDataClust = function(x) {
+  invoke(PredictionClust$new, .args = x)
+}
+
+#' @export
+check_prediction_data.PredictionDataClust = function(pdata) { # nolint
+  assert_row_ids(pdata$row_ids)
+  n = length(pdata$row_ids)
+  assert_integer(pdata$partition, len = n, any.missing = FALSE, null.ok = TRUE)
+
+  prob = pdata$prob
+  if (!is.null(prob)) {
+    # need to check number of columns for matrix
+    assert_matrix(prob, nrows = n)
+    assert_numeric(prob, lower = 0, upper = 1)
+    if (!is.null(rownames(prob))) {
+      rownames(prob) = NULL
+      pdata$prob = prob
+    }
+
+    if (is.null(pdata$partition)) {
+      # calculate partition from prob
+      pdata$partition = max.col(prob, ties.method = "first")
+    }
+  }
+
+  pdata
+}
+
+
+#' @export
+is_missing_prediction_data.PredictionDataClust = function(pdata) { # nolint
+  miss = logical(length(pdata$row_ids))
+
+  if (!is.null(pdata$partition)) {
+    miss = is.na(pdata$partition)
+  }
+
+  if (!is.null(pdata$prob)) {
+    miss = miss | apply(pdata$prob, 1L, anyMissing)
+  }
+
+  pdata$row_ids[miss]
+}
+
+#' @export
+c.PredictionDataClust = function(..., keep_duplicates = TRUE) {
+  dots = list(...)
+  assert_list(dots, "PredictionDataClust")
+  assert_flag(keep_duplicates)
+  if (length(dots) == 1L) {
+    return(dots[[1L]])
+  }
+
+  predict_types = names(mlr_reflections$learner_predict_types$clust)
+  predict_types = map(dots, function(x) intersect(names(x), predict_types))
+  if (!every(predict_types[-1L], setequal, y = predict_types[[1L]])) {
+    stopf("Cannot combine predictions: Different predict types")
+  }
+
+  elems = c("row_ids", "partition")
+  tab = map_dtr(dots, function(x) x[elems], .fill = FALSE)
+  prob = do.call(rbind, map(dots, "prob"))
+
+  if (!keep_duplicates) {
+    keep = !duplicated(tab, by = "row_ids", fromLast = TRUE)
+    tab = tab[keep]
+    prob = prob[keep, , drop = FALSE]
+  }
+
+  result = as.list(tab)
+  result$prob = prob
+
+  set_class(result, "PredictionDataClust")
+}
