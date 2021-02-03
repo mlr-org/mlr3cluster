2382e290283a2b3ac44c691d3e5a266c7b082fe4
Michel Lang <michellang@gmail.com>
2021-02-03 16:57:03 +0100

    move to separate file

--- R/TaskClust.R
+++ R/TaskClust.R
@@ -29,39 +29,3 @@ TaskClust = R6Class("TaskClust",
     }
   )
 )
-
-#' @title Convert to a Cluster Task
-#' @param x (`any`)\cr
-#'   Object to convert, e.g. a `data.frame()`.
-#' @param ... (`any`)\cr
-#'   Additional arguments.
-#' @export
-as_task_clust = function(x, ...) {
-  UseMethod("as_task_clust")
-}
-
-
-#' @rdname as_task_clust
-#' @param clone (`logical(1)`)\cr
-#'   If `TRUE`, ensures that the returned object is not the same as the input `x`.
-#' @export
-as_task_clust.TaskClust = function(x, clone = FALSE, ...) { # nolint
-  if (clone) x$clone() else x
-}
-
-
-#' @rdname as_task_clust
-#' @param id (`character(1)`)\cr
-#'   Id for the new task.
-#'   Defaults to the (deparsed and substituted) name of `x`.
-#' @export
-as_task_clust.data.frame = function(x, id = deparse(substitute(x)), ...) { # nolint
-  TaskClust$new(id = id, backend = x)
-}
-
-
-#' @rdname as_task_clust
-#' @export
-as_task_clust.DataBackend = function(x, id = deparse(substitute(x)), ...) { # nolint
-  TaskClust$new(id = id, backend = x)
-}
