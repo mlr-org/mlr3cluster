59a15b8bed0888243f05b79ef8f92f32bf750b76
Michel Lang <michellang@gmail.com>
2022-08-12 12:45:14 +0200

    fixes for _R_CHECK_DEPENDS_ONLY_

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,9 +1,14 @@
 <%
 lrn = mlr3::lrn(id)
+pkgs = setdiff(lrn$packages, c("mlr3", "mlr3cluster"))
+if (length(pkgs) == 0L) { pkgs = "mlr3" }
+pkgs = paste0(sprintf('requireNamespace("%s")', pkgs), collapse = " && ")
 %>
 #' @examples
-#' learner = mlr3::lrn("<%= id %>")
-#' print(learner)
+#' if (<%= pkgs %>) {
+#'   learner = mlr3::lrn("<%= id %>")
+#'   print(learner)
 #'
-#' # available parameters:
-#' learner$param_set$ids()
+#'   # available parameters:
+#'   learner$param_set$ids()
+#' }
