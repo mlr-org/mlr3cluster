d3d4d19e4c94c18ea1fdbfcd82d38c65d406c05e
damirpolat <damirpolat@protonmail.com>
2022-10-05 21:30:54 -0600

    mclust learner

---
+++ tests/testthat/test_mlr_learners_clust_mclust.R
@@ -0,0 +1,43 @@
+context("clust.mclust")
+
+skip_if_not_installed("mclust")
+
+test_that("autotest", {
+  learner = mlr3::lrn("clust.mclust")
+  expect_learner(learner)
+
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
+
+
+test_that("Learner properties are respected", {
+  task = tsk("usarrests")
+  learner = mlr_learners$get("clust.mclust")
+  expect_learner(learner, task)
+
+  # test on multiple paramsets
+  parset_list = list(
+    list(G = 1:4, modelNames = "EII"),
+    list(initialization = list(noise = 1)),
+    list(G = 3)
+  )
+
+  for (i in seq_along(parset_list)) {
+    parset = parset_list[[i]]
+    learner$param_set$values = parset
+
+    p = suppressWarnings(learner$train(task)$predict(task), classes = "predictionUselessWarning")
+    expect_prediction_clust(p)
+
+    if ("complete" %in% learner$properties) {
+      expect_prediction_complete(p, learner$predict_type)
+    }
+    if ("exclusive" %in% learner$properties) {
+      expect_prediction_exclusive(p, learner$predict_type)
+    }
+    if ("fuzzy" %in% learner$properties) {
+      expect_prediction_fuzzy(p)
+    }
+  }
+})
