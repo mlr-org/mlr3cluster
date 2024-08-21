584c65704e5fae2c45a97c2eb0002a4f8ffc50b6
Lars Kotthoff <larsko@uwyo.edu>
2024-08-21 10:11:28 -0600

    support for empty prediction sets (mlr-org/mlr3#1089)

--- tests/testthat/test_PredictionClust.R
+++ tests/testthat/test_PredictionClust.R
@@ -25,3 +25,27 @@ test_that("filter works", {
   expect_set_equal(pdata$row_ids, 1:3)
   expect_integer(pdata$partition, len = 3)
 })
+
+test_that("construction of empty PredictionDataClust", {
+  task = tsk("usarrests")
+
+  learner = lrn("clust.featureless", predict_type = "partition")
+  learner$train(task)
+  pred = learner$predict(task, row_ids = integer())
+  expect_prediction(pred)
+  expect_set_equal(pred$predict_types, "partition")
+  expect_integer(pred$row_ids, len = 0L)
+  expect_numeric(pred$partition, len = 0L)
+  expect_null(pred$prob)
+  expect_data_table(as.data.table(pred), nrows = 0L, ncols = 2L)
+
+  learner = lrn("clust.featureless", predict_type = "prob")
+  learner$train(task)
+  pred = learner$predict(task, row_ids = integer())
+  expect_prediction(pred)
+  expect_set_equal(pred$predict_types, c("partition", "prob"))
+  expect_integer(pred$row_ids, len = 0L)
+  expect_numeric(pred$partition, len = 0L)
+  expect_numeric(pred$prob, len = 0L)
+  expect_data_table(as.data.table(pred), nrows = 0L, ncols = 3L)
+})
