b8bb483d9ab015f99f2d6afdecff4c55b7e6bb30
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-12-03 10:49:23 +0100

    tests: remove checkmate namespace prefix

--- tests/testthat/helper_expectations.R
+++ tests/testthat/helper_expectations.R
@@ -1,24 +1,22 @@
 expect_prediction_clust = function(p) {
   expect_prediction(p)
-  checkmate::expect_r6(p, "PredictionClust",
+  expect_r6(p, "PredictionClust",
     public = c("row_ids", "truth", "predict_types", "prob", "partition")
   )
-  checkmate::expect_numeric(p$truth, any.missing = TRUE, len = length(p$row_ids), null.ok = TRUE)
-  checkmate::expect_numeric(p$partition,
+  expect_numeric(p$truth, any.missing = TRUE, len = length(p$row_ids), null.ok = TRUE)
+  expect_numeric(p$partition,
     any.missing = FALSE, len = length(p$row_ids),
     null.ok = TRUE
   )
   if ("prob" %in% p$predict_types) {
-    checkmate::expect_matrix(p$prob, "numeric", any.missing = FALSE, nrows = length(p$row_ids))
+    expect_matrix(p$prob, "numeric", any.missing = FALSE, nrows = length(p$row_ids))
   }
 }
 
-expect_task_clust = function(task) {
-  checkmate::expect_r6(task, "TaskClust")
-}
+expect_task_clust = function(task) expect_r6(task, "TaskClust")
 
 expect_prediction_complete = function(p, predict_type) {
-  expect_false(checkmate::anyMissing(p[[predict_type]]))
+  expect_false(anyMissing(p[[predict_type]]))
 }
 
 expect_prediction_exclusive = function(p, predict_type) {
