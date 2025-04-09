32a5a6c30e37a87f1ace9b2ef484bde57ef68d9a
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-04-09 20:49:00 +0200

    refactor: leave blank for empty vector

--- tests/testthat/test_TaskClust.R
+++ tests/testthat/test_TaskClust.R
@@ -2,14 +2,14 @@ test_that("Basic ops on usarrests task", {
   task = tsk("usarrests")
   expect_task(task)
   expect_task_clust(task)
-  expect_identical(task$target_names, character(0L))
+  expect_identical(task$target_names, character())
 })
 
 test_that("Basic ops on ruspini task", {
   task = tsk("ruspini")
   expect_task(task)
   expect_task_clust(task)
-  expect_identical(task$target_names, character(0L))
+  expect_identical(task$target_names, character())
 })
 
 test_that("0 feature task", {
