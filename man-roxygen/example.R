7c16d5a4f0ab45949d4ac48f466fa481ac9126ae
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-09-09 21:56:07 +0200

    docs: modernise example

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,14 +1,7 @@
-<%
-lrn = mlr3::lrn(id)
-pkgs = setdiff(lrn$packages, c("mlr3", "mlr3cluster"))
-if (length(pkgs) == 0L) { pkgs = "mlr3" }
-pkgs = paste0(sprintf('requireNamespace("%s")', pkgs), collapse = " && ")
-%>
-#' @examples
-#' if (<%= pkgs %>) {
-#'   learner = mlr3::lrn("<%= id %>")
-#'   print(learner)
+#' <%= sprintf("@examplesIf mlr3misc::require_namespaces(lrn(\"%s\")$packages, quietly = TRUE)", id) %>
+#' # Define the Learner and set parameter values
+#' <%= sprintf("learner = lrn(\"%s\")", id) %>
+#' print(learner)
 #'
-#'   # available parameters:
-#'   learner$param_set$ids()
-#' }
+#' # available parameters:
+#' learner$param_set$ids()
