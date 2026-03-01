efa06d6e81e4ba859a488848553d553207791a63
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-03-01 14:03:27 +0100

    tests: restructure test helpers

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,16 @@
-library(checkmate)
-library(mlr3)
-library(mlr3cluster)
-library(testthat)
+walk(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+
+generate_tasks.LearnerClust = function(learner, N = 20L) { # nolint
+  set.seed(1L)
+  data = mlbench::mlbench.2dnormals(N, cl = 2L, r = 2, sd = 0.1)
+  task = TaskClust$new("sanity", mlr3::as_data_backend(as.data.frame(data$x)))
+  list(task)
+}
+
+registerS3method("generate_tasks", "LearnerClust", generate_tasks.LearnerClust, envir = parent.frame())
+
+sanity_check.PredictionClust = function(prediction, task, ...) { # nolint
+  prediction$score(measures = msr("clust.silhouette"), task = task) > -1L
+}
+
+registerS3method("sanity_check", "PredictionClust", sanity_check.PredictionClust, envir = parent.frame())
