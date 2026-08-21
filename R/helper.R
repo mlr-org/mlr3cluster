f4b3c9ff5431b4851cae8fca7edf3c94fbe9f712
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-08-21 14:51:08 +0200

    refactor: move Weka helpers to own helper file

--- R/helper.R
+++ R/helper.R
@@ -41,22 +41,3 @@ row_any_na = function(x) {
   }
   rowSums(is.na(x)) > 0L
 }
-
-weka_control = function(pv) {
-  names(pv) = chartr("_", "-", names(pv))
-  invoke(RWeka::Weka_control, .args = pv)
-}
-
-#' @export
-marshal_model.Weka_clusterer = function(model, inplace = FALSE, ...) {
-  rJava::.jcache(model$clusterer)
-  set_class(
-    list(marshaled = model, packages = c("mlr3", "mlr3cluster", "RWeka")),
-    c("Weka_clusterer_marshaled", "marshaled")
-  )
-}
-
-#' @export
-unmarshal_model.Weka_clusterer_marshaled = function(model, inplace = FALSE, ...) {
-  model$marshaled
-}
