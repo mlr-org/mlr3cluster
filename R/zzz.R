a2739a3c3f203ca7fc9b0810499e4dc3d9092893
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-09-12 19:11:56 +0200

    fix: import plot() from graphics

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 #' @import mlr3
 #' @import mlr3misc
 #' @import paradox
+#' @importFrom graphics plot
 #' @importFrom R6 R6Class
 #' @importFrom stats model.frame predict rnorm runif terms
 "_PACKAGE"
